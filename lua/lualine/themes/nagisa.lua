c456361237c94767d7a0af51a483b83687175e98
AzashiroMerume <sanzhikwantadrive@gmail.com>
2024-09-11 11:09:09 +0500

    checking linter

--- lua/lualine/themes/nagisa.lua
+++ lua/lualine/themes/nagisa.lua
@@ -20,44 +20,44 @@ local bg_terminal = colors.bg2
 local bg_command = colors.bg2
 
 nagisa.normal = {
-	a = { fg = colors.fg, bg = colors.blue, gui = "bold" },
-	b = { fg = colors.blue, bg = bg_normal },
-	c = { fg = colors.fg, bg = bg_normal },
+    a = { fg = colors.fg, bg = colors.blue, gui = "bold" },
+    b = { fg = colors.blue, bg = bg_normal },
+    c = { fg = colors.fg, bg = bg_normal },
 }
 
 nagisa.visual = {
-	a = { fg = colors.bg, bg = colors.yellow, gui = "bold" },
-	b = { fg = colors.yellow, bg = bg_normal },
+    a = { fg = colors.bg, bg = colors.yellow, gui = "bold" },
+    b = { fg = colors.yellow, bg = bg_normal },
 }
 
 nagisa.inactive = {
-	a = { fg = colors.fg, bg = colors.bg, gui = "bold" },
-	b = { fg = colors.inactive, bg = bg_normal },
-	c = { fg = colors.inactive, bg = bg_normal },
+    a = { fg = colors.fg, bg = colors.bg, gui = "bold" },
+    b = { fg = colors.inactive, bg = bg_normal },
+    c = { fg = colors.inactive, bg = bg_normal },
 }
 
 nagisa.replace = {
-	a = { fg = colors.fg, bg = colors.red, gui = "bold" },
-	b = { fg = colors.red, bg = bg_replace },
-	c = { fg = colors.fg, bg = bg_normal },
+    a = { fg = colors.fg, bg = colors.red, gui = "bold" },
+    b = { fg = colors.red, bg = bg_replace },
+    c = { fg = colors.fg, bg = bg_normal },
 }
 
 nagisa.insert = {
-	a = { fg = colors.bg, bg = colors.green, gui = "bold" },
-	b = { fg = colors.green, bg = bg_insert },
-	c = { fg = colors.fg, bg = bg_normal },
+    a = { fg = colors.bg, bg = colors.green, gui = "bold" },
+    b = { fg = colors.green, bg = bg_insert },
+    c = { fg = colors.fg, bg = bg_normal },
 }
 
 nagisa.terminal = {
-	a = { fg = colors.fg, bg = colors.green, gui = "bold" },
-	b = { fg = colors.fg, bg = bg_terminal },
-	c = { fg = colors.fg, bg = bg_normal },
+    a = { fg = colors.fg, bg = colors.green, gui = "bold" },
+    b = { fg = colors.fg, bg = bg_terminal },
+    c = { fg = colors.fg, bg = bg_normal },
 }
 
 nagisa.command = {
-	a = { fg = colors.fg, bg = colors.pink, gui = "bold" },
-	b = { fg = colors.pink, bg = bg_command },
-	c = { fg = colors.fg, bg = bg_normal },
+    a = { fg = colors.fg, bg = colors.pink, gui = "bold" },
+    b = { fg = colors.pink, bg = bg_command },
+    c = { fg = colors.fg, bg = bg_normal },
 }
 
 return nagisa
